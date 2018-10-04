d3f9cd4ef3465e603d37e596e3a047ee175601cb
Arun Shah <fastah@gmail.com>
2018-10-04 16:16:25 +0100

    printutil.js

---
+++ www/printutil.js
@@ -0,0 +1,11 @@
+/*global cordova, module*/
+
+module.exports = {
+    send: function (port, baudrate, data, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Sport", "send", [{
+            "port": port,
+            "baudrate": baudrate,
+            "data": data
+        }]);
+    }
+};
